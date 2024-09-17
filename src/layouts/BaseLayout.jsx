71b29fbe209b313e6c93960e85fbd52ff06a667f
sergio jimenez <sjr.social@gmail.com>
2024-09-17 10:40:39 +0200

    Componente SEO en BaseLayout

--- src/layouts/BaseLayout.jsx
+++ src/layouts/BaseLayout.jsx
@@ -1,18 +1,11 @@
-import { Helmet } from "react-helmet-async"
 import Footer from "../components/footer/Footer"
 import Header from "../components/header/Header"
+import Seo from "../components/seo/Seo"
 
 const BaseLayout = ({children, title, description}) => {
   return (
     <>
-      <Helmet>
-        <title>{title}</title>
-        <meta name="description" content={description}/>
-        <meta property="og:title" content={title}/>
-        <meta property="og:description" content={description}/>
-        <meta property="og:url" content={window.location.href}/>
-        <meta property="og:type" content="website"/>
-      </Helmet>
+      <Seo title={title} description={description}/>
       <Header/>
       <main>
         {children}
